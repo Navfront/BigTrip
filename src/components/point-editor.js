5d861d6fc1492cf5b408ebab741003d0e76b8260
Navfront <b3rt@yandex.ru>
2022-05-19 01:09:45 +0300

    editor mock WIP

--- src/components/point-editor.js
+++ src/components/point-editor.js
@@ -1,10 +1,69 @@
+import {
+  EVENTS_TYPE,
+  ALL_POINTS,
+  ALL_DESTINATIONS_BY_TYPE,
+  OFFERS_BY_DESTINATIONS,
+  IMAGES_SRC,
+} from "../mock/events";
+const eventTypes = EVENTS_TYPE;
+const points = ALL_POINTS;
+const destinationsByType = ALL_DESTINATIONS_BY_TYPE;
+const offersByDestinations = OFFERS_BY_DESTINATIONS;
+
+const getEventTypeItemTemplate = (eventType) => {
+  return `
+  <div class="event__type-item">
+  <input id="event-type-${eventType}-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="${eventType}">
+  <label class="event__type-label  event__type-label--${eventType}" for="event-type-${eventType}-1">${eventType}</label>
+</div>
+  `;
+};
+
+const getDestinationOptionTemplate = (destination) => {
+  return `
+  <option value="${destination}"></option>
+  `;
+};
+
+const getOffer = (offer, isChecked = false) => {
+  const offerName = offer.title.split(" ").join("-");
+
+  return `
+  <div class="event__offer-selector">
+  <input class="event__offer-checkbox  visually-hidden" id="event-offer-${offerName}-1" type="checkbox" name="event-offer-${offerName}" ${
+    isChecked ? "checked" : ""
+  }>
+  <label class="event__offer-label" for="event-offer-${offerName}-1">
+    <span class="event__offer-title">${offer.title}</span>
+    &plus;&euro;&nbsp;
+    <span class="event__offer-price">${offer.price}</span>
+  </label>
+</div>
+  `;
+};
+
+const getDestinationImage = (imgSrc) => {
+  return `<img class="event__photo" src="${imgSrc}" alt="Event photo">`;
+};
+
 const pointEditorComponent = () => {
+  let currentType = eventTypes[5];
+  let currentPoint = points[1];
+  let currentDestinationsByType = destinationsByType[0];
+  let currentDestination = currentDestinationsByType[currentType][1];
+  let currentDueDateFrom = "19/03/19 00:00";
+  let currentDueDateTo = "19/03/19 00:00";
+  let currentDescription =
+    "Geneva is a city in Switzerland that lies at the southern tip of expansive Lac Léman (Lake Geneva). Surrounded by the Alps and Jura mountains, the city has views of dramatic Mont Blanc.";
+  let hasOffers = true;
+  let hasDestinations = true;
+
   return `<form class="event event--edit" action="#" method="post">
   <header class="event__header">
     <div class="event__type-wrapper">
       <label class="event__type  event__type-btn" for="event-type-toggle-1">
         <span class="visually-hidden">Choose event type</span>
-        <img class="event__type-icon" width="17" height="17" src="img/icons/flight.png" alt="Event type icon">
+        <img class="event__type-icon" width="17" height="17" src="img/icons/${currentType}.png" alt="Event type icon">
       </label>
       <input class="event__type-toggle  visually-hidden" id="event-type-toggle-1" type="checkbox">
 
@@ -12,50 +71,11 @@ const pointEditorComponent = () => {
         <fieldset class="event__type-group">
           <legend class="visually-hidden">Event type</legend>
 
-          <div class="event__type-item">
-            <input id="event-type-taxi-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="taxi">
-            <label class="event__type-label  event__type-label--taxi" for="event-type-taxi-1">Taxi</label>
-          </div>
-
-          <div class="event__type-item">
-            <input id="event-type-bus-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="bus">
-            <label class="event__type-label  event__type-label--bus" for="event-type-bus-1">Bus</label>
-          </div>
-
-          <div class="event__type-item">
-            <input id="event-type-train-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="train">
-            <label class="event__type-label  event__type-label--train" for="event-type-train-1">Train</label>
-          </div>
-
-          <div class="event__type-item">
-            <input id="event-type-ship-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="ship">
-            <label class="event__type-label  event__type-label--ship" for="event-type-ship-1">Ship</label>
-          </div>
-
-          <div class="event__type-item">
-            <input id="event-type-drive-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="drive">
-            <label class="event__type-label  event__type-label--drive" for="event-type-drive-1">Drive</label>
-          </div>
-
-          <div class="event__type-item">
-            <input id="event-type-flight-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="flight" checked>
-            <label class="event__type-label  event__type-label--flight" for="event-type-flight-1">Flight</label>
-          </div>
-
-          <div class="event__type-item">
-            <input id="event-type-check-in-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="check-in">
-            <label class="event__type-label  event__type-label--check-in" for="event-type-check-in-1">Check-in</label>
-          </div>
-
-          <div class="event__type-item">
-            <input id="event-type-sightseeing-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="sightseeing">
-            <label class="event__type-label  event__type-label--sightseeing" for="event-type-sightseeing-1">Sightseeing</label>
-          </div>
-
-          <div class="event__type-item">
-            <input id="event-type-restaurant-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="restaurant">
-            <label class="event__type-label  event__type-label--restaurant" for="event-type-restaurant-1">Restaurant</label>
-          </div>
+         ${eventTypes
+           .map((it) => {
+             return getEventTypeItemTemplate(it);
+           })
+           .join("")}
         </fieldset>
       </div>
     </div>
@@ -66,18 +86,20 @@ const pointEditorComponent = () => {
       </label>
       <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="Geneva" list="destination-list-1">
       <datalist id="destination-list-1">
-        <option value="Amsterdam"></option>
-        <option value="Geneva"></option>
-        <option value="Chamonix"></option>
+      ${currentDestinationsByType[currentType]
+        .map((it) => {
+          return getDestinationOptionTemplate(it);
+        })
+        .join("")}
       </datalist>
     </div>
 
     <div class="event__field-group  event__field-group--time">
       <label class="visually-hidden" for="event-start-time-1">From</label>
-      <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="19/03/19 00:00">
+      <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="${currentDueDateFrom}">
       &mdash;
       <label class="visually-hidden" for="event-end-time-1">To</label>
-      <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="19/03/19 00:00">
+      <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="${currentDueDateTo}">
     </div>
 
     <div class="event__field-group  event__field-group--price">
@@ -85,78 +107,52 @@ const pointEditorComponent = () => {
         <span class="visually-hidden">Price</span>
         &euro;
       </label>
-      <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="">
+      <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="${
+        currentPoint.base_price
+      }">
     </div>
 
     <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
     <button class="event__reset-btn" type="reset">Cancel</button>
   </header>
-  <section class="event__details">
-    <section class="event__section  event__section--offers">
-      <h3 class="event__section-title  event__section-title--offers">Offers</h3>
+  ${
+    hasOffers
+      ? '<section class="event__details"><section class="event__section  event__section--offers"><h3 class="event__section-title  event__section-title--offers">Offers</h3><div class="event__available-offers"> '
+      : ""
+  }
 
-      <div class="event__available-offers">
-        <div class="event__offer-selector">
-          <input class="event__offer-checkbox  visually-hidden" id="event-offer-luggage-1" type="checkbox" name="event-offer-luggage" checked>
-          <label class="event__offer-label" for="event-offer-luggage-1">
-            <span class="event__offer-title">Add luggage</span>
-            &plus;&euro;&nbsp;
-            <span class="event__offer-price">30</span>
-          </label>
-        </div>
 
-        <div class="event__offer-selector">
-          <input class="event__offer-checkbox  visually-hidden" id="event-offer-comfort-1" type="checkbox" name="event-offer-comfort" checked>
-          <label class="event__offer-label" for="event-offer-comfort-1">
-            <span class="event__offer-title">Switch to comfort class</span>
-            &plus;&euro;&nbsp;
-            <span class="event__offer-price">100</span>
-          </label>
-        </div>
+${
+  hasOffers
+    ? OFFERS_BY_DESTINATIONS.find((it) => typeof it["geneva"] !== "undefined")
+        ["geneva"].map((it, index) => {
+          return getOffer(it, index === 0 || index === 1);
+        })
+        .join("")
+    : ""
+}
 
-        <div class="event__offer-selector">
-          <input class="event__offer-checkbox  visually-hidden" id="event-offer-meal-1" type="checkbox" name="event-offer-meal">
-          <label class="event__offer-label" for="event-offer-meal-1">
-            <span class="event__offer-title">Add meal</span>
-            &plus;&euro;&nbsp;
-            <span class="event__offer-price">15</span>
-          </label>
-        </div>
+  </div>
+  ${hasOffers ? "</section>" : ""}
 
-        <div class="event__offer-selector">
-          <input class="event__offer-checkbox  visually-hidden" id="event-offer-seats-1" type="checkbox" name="event-offer-seats">
-          <label class="event__offer-label" for="event-offer-seats-1">
-            <span class="event__offer-title">Choose seats</span>
-            &plus;&euro;&nbsp;
-            <span class="event__offer-price">5</span>
-          </label>
-        </div>
+  ${
+    hasDestinations
+      ? '<section class="event__section  event__section--destination"><h3 class="event__section-title  event__section-title--destination">Destination</h3><p class="event__destination-description">'
+      : ""
+  }
+    ${hasDestinations ? currentDescription : ""}
+      ${
+        hasDestinations
+          ? '</p><div class="event__photos-container"><div class="event__photos-tape">'
+          : ""
+      }
 
-        <div class="event__offer-selector">
-          <input class="event__offer-checkbox  visually-hidden" id="event-offer-train-1" type="checkbox" name="event-offer-train">
-          <label class="event__offer-label" for="event-offer-train-1">
-            <span class="event__offer-title">Travel by train</span>
-            &plus;&euro;&nbsp;
-            <span class="event__offer-price">40</span>
-          </label>
-        </div>
-      </div>
-    </section>
-
-    <section class="event__section  event__section--destination">
-      <h3 class="event__section-title  event__section-title--destination">Destination</h3>
-      <p class="event__destination-description">Geneva is a city in Switzerland that lies at the southern tip of expansive Lac Léman (Lake Geneva). Surrounded by the Alps and Jura mountains, the city has views of dramatic Mont Blanc.</p>
-
-      <div class="event__photos-container">
-        <div class="event__photos-tape">
-          <img class="event__photo" src="img/photos/1.jpg" alt="Event photo">
-          <img class="event__photo" src="img/photos/2.jpg" alt="Event photo">
-          <img class="event__photo" src="img/photos/3.jpg" alt="Event photo">
-          <img class="event__photo" src="img/photos/4.jpg" alt="Event photo">
-          <img class="event__photo" src="img/photos/5.jpg" alt="Event photo">
-        </div>
-      </div>
-    </section>
+         ${
+           hasDestinations
+             ? IMAGES_SRC.map((it) => getDestinationImage(it))
+             : ""
+         }
+         ${hasDestinations ? "</div></div></section>" : ""}
   </section>
 </form>`;
 };
--- src/mock/events.js
+++ src/mock/events.js
@@ -1,9 +1,8 @@
-export const EVENT_TYPE = [
+export const EVENTS_TYPE = [
   "taxi",
   "bus",
   "train",
   "ship",
-  "transport",
   "drive",
   "flight",
   "check-in",
@@ -57,21 +56,93 @@ export const OFFER = {
   ],
 };
 
-export const LOCAL_POINT = {
-  base_price: 222,
-  date_from: "2019-07-10T22:55:56.845Z",
-  date_to: "2019-07-11T11:22:13.375Z",
-  destination: DESTINATION,
-  is_favorite: false,
-  offers: [
-    {
-      title: "Choose meal",
-      price: 180,
+export const ALL_POINTS = [
+  {
+    id: "0",
+    base_price: 1100,
+    destination: {
+      description:
+        "Chamonix, is a beautiful city, a true asian pearl, with crowded streets.",
+      name: "Chamonix",
+      pictures: [
+        {
+          src: "http://picsum.photos/300/200?r=0.0762563005163317",
+          description: "Chamonix parliament building",
+        },
+      ],
     },
-    {
-      title: "Upgrade to comfort class",
-      price: 50,
+
+    offers: [
+      {
+        title: "Choose meal",
+        price: 180,
+      },
+      {
+        title: "Upgrade to comfort class",
+        price: 50,
+      },
+    ],
+    type: "bus",
+  },
+  {
+    id: "1",
+    base_price: 3100,
+    destination: {
+      description: "Geneva, is a beautiful.",
+      name: "Geneva",
+      pictures: [
+        {
+          src: "http://picsum.photos/300/200?r=0.0762563005163317",
+          description: "Geneva looks good",
+        },
+      ],
     },
-  ],
-  type: "taxi",
-};
+    offers: [
+      {
+        title: "Add luggage",
+        price: 30,
+      },
+      {
+        title: "Switch to comfort class",
+        price: 100,
+      },
+      {
+        title: "Add meal",
+        price: 15,
+      },
+    ],
+    type: "flight",
+  },
+];
+
+export const ALL_DESTINATIONS_BY_TYPE = [
+  { flight: ["Amsterdam", "Geneva", "Chamonix"] },
+];
+
+export const OFFERS_BY_DESTINATIONS = [
+  {
+    geneva: [
+      {
+        title: "Add luggage",
+        price: 30,
+      },
+      {
+        title: "Switch to comfort class",
+        price: 100,
+      },
+      {
+        title: "Add meal",
+        price: 15,
+      },
+    ],
+  },
+];
+
+//WIP
+export const IMAGES_SRC = [
+  "img/photos/1.jpg",
+  "img/photos/2.jpg",
+  "img/photos/3.jpg",
+  "img/photos/4.jpg",
+  "img/photos/5.jpg",
+];
