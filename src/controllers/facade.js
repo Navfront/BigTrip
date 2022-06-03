b49cf44d00419879a4487f21355298d83df09105
Navfront <b3rt@yandex.ru>
2022-06-03 21:05:36 +0300

    add facade

---
+++ src/controllers/facade.js
@@ -0,0 +1,14 @@
+export default class Facade{
+  constructor(tripPresenter) {
+    this._tripPresenter = tripPresenter;
+  }
+
+  init() {
+    this._tripPresenter.renderInfo();
+    this._tripPresenter.renderNavigation();
+    this._tripPresenter.renderAddButton();
+    this._tripPresenter.renderFilter();
+    this._tripPresenter.renderSorts();
+    this._tripPresenter.renderPoints();
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { TEST_POINTS } from './mock/events';
 import TripPresenter from './controllers/trip-presenter';
 import DataModel from './models/data-model';
+import Facade from './controllers/facade';
 
 const headerContainer = document.querySelector('.trip-main');
 const filterContainer = document.querySelector('.trip-controls__filters');
@@ -9,12 +10,7 @@ const eventsContainer = document.querySelector('.trip-events');
 const dataModel = new DataModel;
 dataModel.setPoints(TEST_POINTS);
 
-const tripPresenter = new TripPresenter(headerContainer, filterContainer, eventsContainer, dataModel);
 
-tripPresenter.renderInfo();
-tripPresenter.renderNavigation();
-tripPresenter.renderAddButton();
-tripPresenter.renderFilter();
-tripPresenter.renderSorts();
-tripPresenter.renderPoints();
+const app = new Facade(new TripPresenter(headerContainer, filterContainer, eventsContainer, dataModel));
 
+app.init();
