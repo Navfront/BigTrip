1fbcceb8ddc19b50ac8cc1b262d83308dfce9734
Navfront <b3rt@yandex.ru>
2022-06-12 10:47:35 +0300

    using provider and storage

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import TripPresenter from './controllers/trip-presenter';
 import DataModel from './models/data-model';
 import Facade from './controllers/facade';
 import Api from './api';
-import { EVENTS_DATA, EVENT_DESTINATIONS, POINTS } from './mock/points';
+import { EVENTS_DATA, EVENT_DESTINATIONS, POINTS, MOD_POINTS } from './mock/points';
+import Storage from './storage';
+import Provider from './provider';
 
 
 const ADRESS = {
@@ -21,11 +23,16 @@ const filterContainer = document.querySelector('.trip-controls__filters');
 const eventsContainer = document.querySelector('.trip-events');
 
 const api = new Api(ADRESS);
+const storage = new Storage;
+const provider = new Provider(api, storage);
 
 const dataModel = new DataModel();
-// dataModel.setDestinations(EVENT_DESTINATIONS);
-// dataModel.setEvents(EVENTS_DATA);
-// dataModel.setPoints(POINTS);
+
+Promise.all([provider.getDestinations(), provider.getEvents(), provider.getAllPoints()]).then((all) => {
+  dataModel.setDestinations(all[0].message);
+  dataModel.setEvents(all[1].message);
+  dataModel.setPoints(all[2].message);
+});
 
 const presenter = new Facade(new TripPresenter(headerContainer, filterContainer, eventsContainer, dataModel));
 presenter.init();
